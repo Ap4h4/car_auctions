213f28afaf609ee208dd30f27b10303337ff6e65
Paplosk <p.ploskonka93@gmail.com>
2026-03-27 15:43:58 +0100

    playwright browser function added

--- scrapping/scrapping.py
+++ scrapping/scrapping.py
@@ -103,22 +103,24 @@ def debt_auctions_get_all_links(page):
 
     return all_links
 
-
-def debt_auctions_scrapper_v2():
-    """
-    New version of scrapping debt auctions page
-    """
-    logger.info("Starting scrapping debt_auctions page")
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        context = browser.new_context(
+def playwright_browser_init():
+    browser = p.chromium.launch(headless=True)
+    context = browser.new_context(
             viewport={"width": 1920, "height": 1080},
             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
             java_script_enabled=True,
             locale="pl-PL",
             timezone_id="Europe/Warsaw",
             )
-        page = context.new_page()
+    page = context.new_page()
+    return browser,page
+def debt_auctions_scrapper_v2():
+    """
+    New version of scrapping debt auctions page
+    """
+    logger.info("Starting scrapping debt_auctions page")
+    with sync_playwright() as p:
+        browser,page = playwright_browser_init()
         all_links = debt_auctions_get_all_links(page)
         logger.info(f"{len(all_links)} debt auctions found. Starting iteration over them...")
 
@@ -311,8 +313,7 @@ def cleaning_brands(brands):
 
 def scrapping_brands():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        page = browser.new_page()
+        browser,page = playwright_browser_init()
 
         page.goto("https://www.otomoto.pl/osobowe", timeout=60_000)
 
@@ -350,8 +351,7 @@ def cleaning_models(models):
 
 def scrapping_models(brand):
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        page = browser.new_page()
+        browser,page = playwright_browser_init()
 
         page.goto("https://www.otomoto.pl/osobowe/"+brand+"", timeout=60_000)
 
@@ -422,8 +422,7 @@ def get_otomoto_raw_cars_auctions(brand, model, year):
     
     with sync_playwright() as p:
         
-        browser = p.chromium.launch(headless=True)
-        page = browser.new_page()
+        browser,page = playwright_browser_init()
         page.goto(url, wait_until="networkidle")
         if not page:
             logger.error("Missing page - otomoto not rendered")
