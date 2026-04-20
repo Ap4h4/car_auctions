347624706fb08e2da86d394b03679245bce4794a
Paplosk <p.ploskonka93@gmail.com>
2026-04-20 16:58:34 +0200

    Adding cleaning outdated auctions with postgress SP

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import datetime
 from scrapping.scrapping import debt_auctions_scrapper_v2, get_raw_cars_brand_models,get_raw_cars_brands
 from scrapping.sparql_wikidata import fetch_wikidata, build_dictionary
 from db_connectors.mongo_db_connector import mongo_bulk_upload,mongo_truncate_collection, connect_to_db as mongo_connect_to_db,get_all_auctions
-from db_connectors.postgressql_connector import connect_to_db as pg_connect_to_db, pg_insert_car_makes, pg_insert_car_models,pg_insert_car_auctions, get_cars_brands, get_car_brand_models,get_car_brand_model_ids
+from db_connectors.postgressql_connector import connect_to_db as pg_connect_to_db, pg_insert_car_makes, pg_insert_car_models,pg_insert_car_auctions,pg_clean_up_auctions, get_cars_brands, get_car_brand_models,get_car_brand_model_ids
 from config.logging_config import setup_logging
 from etl.debt_auctions_processing import auction_brands_enriched_output_list, auction_brand_model_enriched, preparing_pg_auction_input_list,otomoto_auctions
 
@@ -55,6 +55,7 @@ def run_auctions_enrichment():
     output_brand_list = auction_brands_enriched_output_list(auctions_dict,car_all_brands)
     output_list = preparing_pg_auction_input_list (output_brand_list)
     pg_insert_car_auctions(output_list)
+    pg_clean_up_auctions()
     #getting otomoto auctions statistics for debt auctions
     otomoto_auctions()
     logger.info("Completing auction enrichment function")
